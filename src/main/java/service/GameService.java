0a1d3e05f70d3502f7f9a09ad8c8ea56711ed73f
Kusakina <dashakys01@gmail.com>
2022-04-04 12:48:06 +0400

    доделала Сервисы

--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -4,7 +4,7 @@ import model.Game;
 import model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class GameService extends AbstractEntityService<GameService, JpaRepository<Game, Long>> {
+public class GameService extends AbstractEntityService<Game, JpaRepository<Game, Long>> {
     private Game selectedGame;
     public GameService(JpaRepository<Game, Long> repository) {
         super(repository);
