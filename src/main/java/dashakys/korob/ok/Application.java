39e4840c48c588fe3a24b6dc0a4ac709ff8bf8f1
Dasha Kusakina <dashakys01@gmail.com>
2022-04-05 00:19:44 +0300

    Настроила запуск приложения в консоли

--- src/main/java/dashakys/korob/ok/Application.java
+++ src/main/java/dashakys/korob/ok/Application.java
@@ -1,13 +1,22 @@
 package dashakys.korob.ok;
 
+import dashakys.korob.ok.service.GameService;
+import dashakys.korob.ok.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+@RequiredArgsConstructor
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello world: "  + args.length);
+	}
 }
