1c35c09af0aecacc8640c5f62861c0013023a31d
Viacheslav Muravev <muravjevslava@gmail.com>
2022-04-05 13:20:05 +0300

    Передала внутрь EntityServiceException исключения-причины

--- src/main/java/dashakys/korob/ok/service/EntityServiceException.java
+++ src/main/java/dashakys/korob/ok/service/EntityServiceException.java
@@ -2,10 +2,13 @@ package dashakys.korob.ok.service;
 
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 public class EntityServiceException extends RuntimeException {
 
     public EntityServiceException(String message) {
         super(message);
+    }
+
+    public EntityServiceException(Throwable cause) {
+        super(cause);
     }
 }
