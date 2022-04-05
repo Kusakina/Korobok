a8bf625d5dbb4fa0ec9733c3b7e3f20d37a29475
Dasha <dashakys01@gmail.com>
2022-04-05 16:49:07 +0300

    GameService: addGame

--- src/main/java/dashakys/korob/ok/service/GameService.java
+++ src/main/java/dashakys/korob/ok/service/GameService.java
@@ -1,10 +1,12 @@
 package dashakys.korob.ok.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 
 import dashakys.korob.ok.model.Game;
+import dashakys.korob.ok.model.Profile;
 import dashakys.korob.ok.repository.GameRepository;
 import org.springframework.stereotype.Service;
 
@@ -26,11 +28,26 @@ public class GameService extends AbstractEntityService<Game, GameRepository> {
         return selectedGame;
     }
 
-    public Optional<Game> findByName(String name) {
+    private Optional<Game> findByName(String name) {
         try {
             return repository.findByName(name);
         } catch (Exception e) {
             throw new EntityServiceException(e);
         }
+    }
+
+    public Game addGame(String name) {
+        if (name.isBlank()) {
+            throw new EntityServiceException("Название игры не должно состоять только из пробельных символов");
+        }
+
+        if (findByName(name).isPresent()) {
+            throw new EntityServiceException(String.format("Игра с названием %s уже сущеcтвует", name));
+        }
+
+        Game game = new Game(name);
+        save(game);
+
+        return game;
     }
 }
