29e6f134342404ebb059b9c65134e11f18e10a07
Dasha <dashakys01@gmail.com>
2022-04-30 21:39:13 +0300

    Added query for findByLogin

--- src/main/java/dashakys/korob/ok/repository/ProfileRepository.java
+++ src/main/java/dashakys/korob/ok/repository/ProfileRepository.java
@@ -3,6 +3,8 @@ package dashakys.korob.ok.repository;
 import dashakys.korob.ok.model.Credentials;
 import dashakys.korob.ok.model.Profile;
 import dashakys.korob.ok.model.Role;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,10 @@ import java.util.Optional;
 
 @Repository
 public interface ProfileRepository extends EntityRepository<Profile> {
-    Optional<Profile> findByLogin(String login);
+    @Query("SELECT cr.profile FROM Credentials cr " +
+            "WHERE cr.login = :login"
+    )
+    Optional<Profile> findByLogin(@Param("login") String login);
     Optional<Profile> findByCredentials(Credentials credentials);
     List<Profile> findAllByRole(Role role);
 }
